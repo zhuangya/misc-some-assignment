4978d9231ec546b917f1c80dc652a4cce169db87
Ya Zhuang <zhuangya@gmail.com>
2016-03-27 06:28:35 +0800

    add: csscomb and autoprefixer gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 
+var $ = require('gulp-load-plugins')();
+
 const path = require('path');
 
 const appPath = path.join(__dirname, 'app/');
@@ -16,4 +18,20 @@ gulp.task('serve', _ => {
   gulp.watch(`${appPath}/**/*`, browserSync.reload);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('styles', function () {
+  return gulp.src(`${appPath}/css/**/*.css`)
+    .pipe($.autoprefixer([
+      'Android 2.3',
+      'Android >= 4',
+      'Chrome >= 20',
+      'Firefox >= 24',
+      'Explorer >= 9',
+      'iOS >= 6',
+      'Opera >= 12',
+      'Safari >= 6'
+    ]))
+    .pipe($.csscomb())
+    .pipe(gulp.dest(`${appPath}/dist/css`));
+});
+
+gulp.task('default', ['styles', 'serve']);
